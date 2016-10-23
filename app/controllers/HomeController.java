6504de1d86da1bc2d68a3602b70c93a0620d0ff6
Live session user <sandeep.k@betsol.com>
2016-10-23 11:03:50 +0000

    All three lamdaaas

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import models.ConnectionRequest;
 import models.Profile;
 import models.User;
 import play.mvc.Controller;
@@ -30,7 +31,7 @@ public class HomeController extends Controller {
         data.put("company",profile.company);
         data.set("connections",objectMapper.valueToTree(user.connections.stream().map(connection->{
             ObjectNode connectionJson=objectMapper.createObjectNode();
-            User connectionJsonUser=User.find.byId(id);
+            User connectionJsonUser=User.find.byId(connection.id);
             Profile connectionJsonProfile=Profile.find.byId(user.profile.id);
             connectionJson.put("id",connectionJsonUser.id);
             connectionJson.put("firstName",connectionJsonProfile.firstName);
@@ -39,6 +40,30 @@ public class HomeController extends Controller {
             connectionJson.put("company",connectionJsonProfile.company);
             return connectionJson;
         }).collect(Collectors.toList())));
+        data.set("connectionRequests",objectMapper.valueToTree
+                (user.connectionRequestsReceived.stream().filter
+                        (connectionRequestCheck->connectionRequestCheck.status.equals
+                            (ConnectionRequest.Status.WAITING)).map(connectionRequest->{
+            ObjectNode connectionRequestJson=objectMapper.createObjectNode();
+            User connectionRequestUser=connectionRequest.sender;
+            Profile connectionRequestProfile=Profile.find.byId(connectionRequestUser.profile.id);
+            connectionRequestJson.put("id",connectionRequest.id);
+            connectionRequestJson.put("firstName",connectionRequestProfile.firstName);
+            return connectionRequestJson;
+        }).collect(Collectors.toList())));
+        data.set("suggestions",objectMapper.valueToTree(User.find.all().stream()
+                .filter(x->!user.equals(x))
+                .filter(x->!user.connections.contains(x))
+                .filter(x->!user.connectionRequestsReceived.stream()
+                        .map(y->y.sender).collect(Collectors.toList()).contains(x))
+                .filter(x->!user.connectionRequestsSent.stream()
+                        .map(z->z.receiver).collect(Collectors.toList()).contains(x)).map(suggestion->{
+            ObjectNode suggestionJson=objectMapper.createObjectNode();
+            Profile suggestionJsonProfile= suggestion.profile;
+            suggestionJson.put("firstName",suggestionJsonProfile.firstName);
+            suggestionJson.put("email",suggestion.email);
+            return suggestionJson;
+                }).collect(Collectors.toList())));
 
         return ok();
     }
---
+++ app/controllers/RequestController.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import models.ConnectionRequest;
+import models.User;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * Created by lubuntu on 10/23/16.
+ */
+public class RequestController extends Controller {
+    public Result sendRequest(Long senderId,Long receiverId){
+        if(senderId == null || receiverId == null || User.find.byId(senderId)== null || User.find.byId(receiverId)==null) {
+
+            return ok();
+        }
+        else {
+            ConnectionRequest request = new ConnectionRequest();
+            request.sender.id = senderId;
+            request.receiver.id = receiverId;
+            request.status = ConnectionRequest.Status.WAITING;
+            ConnectionRequest.db().insert(request);
+        }
+        return ok();
+    }
+    public Result acceptRequest(Long requestId){
+        if(requestId==null || ConnectionRequest.find.byId(requestId)==null){
+            return ok();
+        }
+        else {
+            ConnectionRequest request = ConnectionRequest.find.byId((requestId));
+            request.sender.connections.add(request.receiver);
+            request.receiver.connections.add(request.sender);
+            request.status = ConnectionRequest.Status.ACCEPTED;
+            User.db().save(request.sender);
+            User.db().save(request.receiver);
+        }
+        return ok();
+
+        }
+
+    }
+
+
+}
